47c851afc155b20e8ed061c3ec08d9e3c2aa790a
Zhao <zhaoxb@sobot.com>
2016-06-02 11:35:04 +0800

    develop

--- gulp/task/browerify.js
+++ gulp/task/browerify.js
@@ -1,14 +1,13 @@
 var gulp = require("gulp");
 var browserify = require('gulp-browserify');
-var config = require('../config.json');
 var notify = require('gulp-notify');
 gulp.task('browserify',function(){
-	gulp.src(config.baseDir+'js/**/entrance.js').
+	gulp.src('./**/entrance.js').
 	pipe(browserify({
 		'insertGlobals':false
 	})).
 	on("error",notify.onError({
 		"message":"Error: <%= error.message %>"
 	})).
-	pipe(gulp.dest(config.baseDir+'dest/js'));
+	pipe(gulp.dest('./dest'));
 });
