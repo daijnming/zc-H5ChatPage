81f6f1113f802a1cd3ed2b392b1c74feee0e952d
gouding <gdenzel@163.com>
2016-06-06 14:51:59 +0800

    develop

--- wap/js/core/core.js
+++ wap/js/core/core.js
@@ -1,7 +1,42 @@
 var Core = function() {
     var that = {};
     var promise = require('../../../common/initConfig.js')();
+    //api接口
+    var api = {
+      keepDetail_url:'/chat/user/getChatDetailByCid.action'
+    };
+    var initConfig = function(data){
+
+      //FIXME 页面初始化
+
+
+      //FIXME 初始化类型
+      data.apiInit.ustatus=-1;
+      //用户当前状态 -2 排队中； -1 机器人； 0 离线； 1 在线；
+      if(data.apiInit.ustatus == 1 || data.apiInit.ustatus == -2) {
+          //更新会话保持标识
+          data.flags.isKeepSessions = true;
+      } else if(data.apiInit.ustatus == -1) {
+          //拉取会话记录
+          $.ajax({
+            type : "post",
+            url : api.keepDetail_url,
+            dataType : "json",
+            data: {
+                cid: data.apiInit.cid,
+                uid: data.apiInit.uid
+            },
+            success:function(data){
+              console.log(data);
+              promise.resolve();
+            }
+          });
+      } else {
+          //处理客服类型 机器人、人工、邀请模式
+      }
+    };
     promise.then(function(data) {
+        initConfig(data);
         $(document.body).trigger("core.onload",[{
           data:data
         }]);
