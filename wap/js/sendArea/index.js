f8398cb3e87b70cab39aa97bbf8c1c5c79c74689
XiaoDai <15712897291@163.com>
2016-06-12 16:09:30 +0800

    adsf

--- wap/js/sendArea/index.js
+++ wap/js/sendArea/index.js
@@ -8,7 +8,8 @@ function TextArea(node,core,window) {
     var listener = require("../../../common/util/listener.js");
     var loadFile = require('../../../common/util/load.js')();
     //表情
-    //var ZC_Face = require('./qqFace.js');
+    var ZC_Face = require('./qqFace.js')();
+
     //上传附件
     /*var uploadImg = require('./uploadImg.js');
      var inputCache = {};
@@ -22,11 +23,13 @@ function TextArea(node,core,window) {
         answer;
     //传给聊天的url
     var parseDOM = function() {
+        $chatArea=$(".js-chatArea");
         $sendBtn = $(".js-sendBtn");
         $textarea = $(".js-textarea");
         $add = $(".js-add");
         $chatAdd = $(".js-chatAdd");
         $emotion = $(".js-emotion");
+        $tab=$(".js-tab");
     };
     var onImageUpload = function(evt,data) {
         //onFileTypeHandler(data);
@@ -73,7 +76,7 @@ function TextArea(node,core,window) {
         var str =$textarea.html();
         //判断输入框是否为空
         if(str.length == 0 || /^\s+$/g.test(str)) {
-            $sendMessage.val("")
+            $textarea.html("")
             return false;
         } else {
             //通过textarea.send事件将用户的数据传到显示台
@@ -84,49 +87,90 @@ function TextArea(node,core,window) {
                 'date' : +new Date()
             }]);
         }
-        $sendMessage.val("");
+        $textarea.html("");
         //清空待发送框
     };
-    var showAddHandler=function(){
-        if($chatAdd.hasClass("showChatAdd")){
-            $chatAdd.removeClass("showChatAdd");
-            $chatAdd.animate({
-                height : "1px"
+    var showChatAreaHandler=function(){
+        if($chatArea.hasClass("showChatArea")){
+            $chatArea.removeClass("showChatArea");
+            $chatArea.animate({
+                bottom : "-215px"
             },200);
         } else {
-            $chatAdd.addClass("showChatAdd");
-            $chatAdd.animate({
-                height : "215px"
+            $chatArea.addClass("showChatArea");
+            $chatArea.animate({
+                bottom : "0px"
             },200)
         }
 
     };
-    var hideChatAddHandler = function() {
-        $chatAdd.removeClass("showChatAdd");
-        $chatAdd.animate({
-            height : "1px"
+    var hideChatAreaHandler = function() {
+        $chatArea.removeClass("showChatArea");
+        $chatArea.animate({
+            bottom : "-215px"
         },200);
+    };
+    //表情、加号切换
+    var tabChatAreaHandler=function(){
+        var id=$(this).attr("data-id");
+        $(".tab-active").hide();
+        $(id).show();
+    };
+     //定位光标
+    var gotoxyHandler=function(evt,data){
+        console.log(data);
+        var src=data.answer;
+         //将新表情追加到待发送框里
+        var oTxt1 = document.getElementById("js-textarea");
+        var cursurPosition=-1;
+        if(oTxt1.selectionStart||oTxt1.selectionStart==0){//非IE浏览器
+            cursurPosition= oTxt1.selectionStart;
+        }else{//IE
+           var range = document.selection.createRange();
+            range.moveStart("character",-oTxt1.value.length);
+            cursurPosition=range.text.length;
+        }
+        var currentSaytextBefore=$textarea.html().substring(0,cursurPosition);
+        var currentSaytextAfter=$textarea.html().substring(cursurPosition);
+        var currentSaytext=currentSaytextBefore+src+currentSaytextAfter;
+        $($sendMessage).val(currentSaytext);
+        //定位光标
+        var pos=(currentSaytextBefore+src).length;
+        if(oTxt1.setSelectionRange)
+            {
+                oTxt1.setSelectionRange(pos,pos);
+                oTxt1.focus();
+            }
+            else if (oTxt1.createTextRange) {
+                var range = oTxt1.createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
     };
     var bindLitener = function() {
         //发送按钮
         $sendBtn.on("click",onbtnSendHandler);
-        /*
-         *
-         qq表情
-         */
+        //qq表情
         $emotion.on("click",onEmotionClickHandler);
         $textarea.on("keyup",showSendBtnHandler);
-        $textarea.on("focus",hideChatAddHandler);
-        $add.on("click",showAddHandler);
+        $textarea.on("focus",hideChatAreaHandler);
+        $add.on("click",showChatAreaHandler);
+        $emotion.on("click",showChatAreaHandler);
+        $tab.on("click",tabChatAreaHandler)
+        //定位光标
+        listener.on("textarea.gotoxy",gotoxyHandler);
     };
     var initFace = function() {
-       // ZC_Face(); 
+       //ZC_Face(); 
     };
     var onEmotionClickHandler = function() {
-      //  ZC_Face.show();
+       //ZC_Face.show();
+       listener.trigger('sendArea.faceShow');
     };
     var initPlugsin = function() {//插件 
-
+        initFace();
         //uploadFun = uploadImg($uploadBtn,node,core,window);
         //上传图片
 
@@ -143,7 +187,6 @@ function TextArea(node,core,window) {
         global = data;
         currentUid=global.uid;
         currentCid=global.cid;
-        console.log(data);
         init();
     });
 
