b7c250bd207a36c795c91c6dac9b6ebc03025727
Zhao <zhaoxb@sobot.com>
2016-06-16 12:00:59 +0800

    Develop

--- wap/js/fun/closesession.js
+++ wap/js/fun/closesession.js
@@ -1,3 +1,15 @@
-(function(){
-	
+(function() {
+    var listener = require('../../../common/util/listener.js');
+    var bindListener = function() {
+        listener.on("core.sessionclose", function() {
+            alert("客服把你踢了");
+            window.location.reload();
+        });
+    };
+
+    var init = function() {
+        bindListener();
+    };
+
+    init();
 })();
