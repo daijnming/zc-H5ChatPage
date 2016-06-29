fd593de496e6c133a5dd1ebabe4b6984bd294421
Zhao <zhaoxb@sobot.com>
2016-06-29 16:50:16 +0800

    Devleop

---
+++ gulp/task/useref-wap.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var useref = require("gulp-useref");
+var notify = require('gulp-notify');
+var gulpIf = require("gulp-if");
+var cleanCSS = require('gulp-clean-css');
+
+var autoPrefixed = require("gulp-autoprefixer");
+gulp.task("useref-wap",['browserify-wap'],function(){
+	return gulp.src("wap/index.html").
+	pipe(useref()).
+	pipe(gulpIf("*.css",cleanCSS({
+		'compatibility':"ie8"
+	}))).
+	on("error",notify.onError({
+		"message":"Error: <%= error.message %>"
+	})).
+	pipe(gulpIf("*.js",uglify())).
+	on("error",notify.onError({
+		"message":"Error: <%= error.message %>"
+	})).
+	pipe(gulp.dest('./dest/wap'));
+});
