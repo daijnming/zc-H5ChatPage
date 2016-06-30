abb7c6e543ff1e21e8d6f9ee81e89968b4448d96
Zhao <zhaoxb@sobot.com>
2016-06-30 16:43:51 +0800

    develop

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 CONF="WEB-INF/replace.conf"
 JAR="WEB-INF/lib/replace.jar"
+TYPE=
+TEST=
 while [ $# -gt 0 ]
 do
 	case $1 in 
 	-p )
 	 	case $2 in
 			wap )
-				gulp production-wap
-				find dist/wap -name "*.html" -type f | xargs -I {}  sh replace.sh {} $JAR $CONF 
+				TYPE="wap"
 			;;
 			pc )
 			;;
@@ -17,9 +18,16 @@ do
 			;;
 		esac
 	;;
+	-t)
+		TEST="-T"
+	;;
 	esac
 	shift;
 done
-
-
+if [ "$TYPE"x = "wap"x ];then
+	gulp production-wap
+	if [ "$TEST"x = ""x ];then
+		find dist/wap -name "*.html" -type f | xargs -I {}  sh replace.sh {} $JAR $CONF 
+	fi
+fi
 
